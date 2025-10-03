38f6b9bf070a23d458972a434a5633eee0fd2d1c
kuszyp <piotr@myapp.pl>
2025-10-03 17:25:19 +0200

    chore: add recipies from lazyvim repository

--- nvim/.config/nvim/lua/plugins/formatting.lua
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -19,10 +19,6 @@ return {
         fish = { "fish_indent" },
         sh = { "shfmt" },
       },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
     },
   },
 
@@ -37,6 +33,77 @@ return {
         "prettier",
         "black",
         "isort",
+      },
+    },
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          elseif vim.snippet.active({ direction = 1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(1)
+            end)
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.snippet.active({ direction = -1 }) then
+            vim.schedule(function()
+              vim.snippet.jump(-1)
+            end)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    init = function()
+      vim.g.cmp_disabled = false -- Set the default state to enabled
+    end,
+    keys = {
+      {
+        "<leader>uA", -- Keybinding to toggle nvim-cmp
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled -- Toggle the state
+
+          -- Update the nvim-cmp configuration dynamically
+          require("cmp").setup({
+            enabled = function()
+              return not vim.g.cmp_disabled
+            end,
+          })
+
+          -- Notify the user of the current state
+          local msg = vim.g.cmp_disabled and "Autocompletion (cmp) disabled" or "Autocompletion (cmp) enabled"
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = "Toggle autocompletion",
       },
     },
   },
